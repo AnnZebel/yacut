4f5f79bf7fa707865c246d89c1178f710068f74e
Аня Зыбель <ann@zebel.by>
2024-08-13 21:30:52 +0300

    fix yacut

--- yacut/constants.py
+++ yacut/constants.py
@@ -2,5 +2,7 @@ import string
 
 SYMBOLS = string.ascii_letters + string.digits
 SHORT_ID_LENGTH = 6
+MAX_URL_LENGTH = 256
+MAX_SHORT_ID_LENGTH = 16
 LINK_REG = r'^[a-zA-Z\d]{1,16}$'
 FIELD_NAMES = {'original': 'url', 'short': 'custom_id'}
--- yacut/models.py
+++ yacut/models.py
@@ -3,13 +3,14 @@ from datetime import datetime
 from flask import url_for
 
 from yacut import db
-from .constants import FIELD_NAMES
+from .constants import FIELD_NAMES, MAX_URL_LENGTH, MAX_SHORT_ID_LENGTH
 
 
 class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    original = db.Column(db.String(256), nullable=False)
-    short = db.Column(db.String(16), unique=True, nullable=False)
+    original = db.Column(db.String(MAX_URL_LENGTH), nullable=False)
+    short = db.Column(db.String(MAX_SHORT_ID_LENGTH),
+                      unique=True, nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def from_dict(self, data):
